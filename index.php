7bac4f2842e850c686715d2a7a093636e6dcb350
luishi <luisin_sardinas@hotmail.com>
2019-04-26 00:40:46 -0400

    Sección de cursos terminado, probar menu final con base negra estilo nextU

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
     <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <title>Sapiens</title>
 </head>
 <body>
@@ -34,13 +35,9 @@
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Cursos
                     </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-
-                        <a class="dropdown-item" href="#"></a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">  
+                        <a class="dropdown-item" href="#">Curso</a>
                         <div class="dropdown-divider"></div>
-
-
-
                         <a class="dropdown-item" href="#">DERECHO</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="#">ELABORACIÓN DE TESIS</a>
@@ -95,42 +92,145 @@
                 <button type="submit" class="btn">Enviar</button>
             </form>
         </div>
+        <div class="verAbajo">
+            <h3>Sigue viendo</h3>
+            <h3><i class="fas fa-angle-double-down"></i></h3>
+        </div>
     </div>
 
-    <div class="container-fluid" id="contenido">
+    <div class="container" id="contenido">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1>Cursos Disponibles</h1>
+                <p>Conviertete en un experto en el área que prefieras.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="https://www.mistica.es/images/img/estructura/aula-magna-asientos.jpg" class="card-img-top color1">
+                    <div class="card-body">
+                        <h5 class="card-title">Derecho</h5>
+                        <a href="#" class="btn btn-primary">Conoce más</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="https://www.mistica.es/images/img/estructura/aula-magna-asientos.jpg" class="card-img-top color2">
+                    <div class="card-body">
+                        <h5 class="card-title">Administración de empresas</h5>
+                        <a href="#" class="btn btn-primary">Conoce más</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="https://www.mistica.es/images/img/estructura/aula-magna-asientos.jpg" class="card-img-top color3">
+                    <div class="card-body">
+                        <h5 class="card-title">ELABORACIÓN de tesis</h5>
+                        <a href="#" class="btn btn-primary">Conoce más</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="https://www.mistica.es/images/img/estructura/aula-magna-asientos.jpg" class="card-img-top color3">
+                    <div class="card-body">
+                        <h5 class="card-title">ELABORACIÓN de tesis</h5>
+                        <a href="#" class="btn btn-primary">Conoce más</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container" id="porque">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">¿Por que Sapiens?</h1>
+            </div>
+        </div>
         <div class="row">
             <div class="col-md-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                            content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="container-fluid contenedor">
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="icono">
+                                <i class="icono far fa-clock"></i>
+                            </div>
+                        </div>
+                        <div class="col-8">
+                            <p>Cursos rápidos, directo a lo que necesitas</p>
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                            content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="container-fluid contenedor">
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="icono">
+                                <i class="icono fas fa-chart-line"></i>
+                            </div>
+                        </div>
+                        <div class="col-8">
+                            <p>Mejora tus conocimientos e incrementa tus posibilidades de conseguir mejores puestos.</p>
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                            content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="container-fluid contenedor">
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="icono">
+                                <i class="icono far fa-thumbs-up"></i>
+                            </div>
+                        </div>
+                        <div class="col-8">
+                            <p>Facilidad de aprendizaje, los docentes son especializados en esta área con metodologías de enseñanza aptas para todo publico</p>
+                        </div>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-center">Nosotros</h2>
+            </div>
+            <div class="col-md-5">
+                Somos una organización de distintas áreas con Bastánte experiencia en el mercado laboral. <br>
+                Conoce a nuestros docentes especializados en el área y personas que hicieron toda esta institución posible. <br>
+                <h3><a href="" class="btn btn-danger">Conoce mas de cerca a nuestros docentes</a></h3>
+            </div>
+            <div class="col-md-7">
+                <a href="">
+                    <i class="fab fa-whatsapp"></i> <br>
+                    Envianos un mensaje por WhatsApp haciendo clicl aqui <br>
+                </a>
+                <a href="">
+                    <i class="fab fa-facebook"></i> <br>
+                    Conoce nuestra página de facebook y encuentra contenido de interes <br>
+                </a>
+                <a href="">
+                    <i class="fab fa-facebook-messenger"></i> <br>
+                    Envianos un mensaje por Messenger, atenderemos tus dudas, consultas y sugerencias que nos quieras dar. <br>
+                </a>
+                
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-center">Como llegar</h2>
+            </div>
+            <div class="col-md-5">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d956.40901951025!2d-68.1372791708302!3d-16.493954999288746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTbCsDI5JzM4LjIiUyA2OMKwMDgnMTIuMiJX!5e0!3m2!1ses!2sbo!4v1556199659805!5m2!1ses!2sbo" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+            <div class="col-md-5">
             </div>
         </div>
     </div>
